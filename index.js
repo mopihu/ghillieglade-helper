74b930f6ba8aa492f2b20a468d749741c7cb0cef
m0py <mopihu@gmail.com>
2018-12-16 00:07:55 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ module.exports = function GhilliegladeHelper(mod) {
     return true;
   });
 
-  mod.hook('S_SPAWN_NPC', 9, event => {
+  mod.hook('S_SPAWN_NPC', 10, event => {
     if (!enabled) return;
     if (event.huntingZoneId == 713 && chestIds.includes(event.templateId)) {
       reset = true;
